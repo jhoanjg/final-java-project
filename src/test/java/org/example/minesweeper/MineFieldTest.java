6e31828c394486da8695a4ee85d1dd3a3975b7b5
jhoanjg <jhoan@dataico.com>
2025-02-18 10:30:14 -0500

    add tests

--- src/test/java/org/example/minesweeper/MineFieldTest.java
+++ src/test/java/org/example/minesweeper/MineFieldTest.java
@@ -1,7 +1,33 @@
 package org.example.minesweeper;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+//Arrange
+//Act
+//Assert
 
 public class MineFieldTest {
+    @Test
+    public void creates_a_10x10_grid() {
+        MineField n;
 
+        n = new MineField();
+
+        assertEquals(10, n.cells.length);
+        assertEquals(10, n.cells[0].length);
+    }
+
+    @Test
+    public void cells_are_hidden() {
+        MineField n;
+
+        n = new MineField();
+
+        for (int x = 0; x < n.cells.length; x++) {
+            for (int y = 0; y < n.cells[x].length; y++) {
+                assertTrue(n.cells[0][0].isHidden);
+            }
+        }
+    }
 }
