7a7a443359cc5f5ff6fe19f9e825edfa3546809c
Tony Kay <tony.kay@gmail.com>
2025-02-18 06:07:31 -0800

    Updated files.

---
+++ src/main/java/org/example/minesweeper/Main.java
@@ -0,0 +1,4 @@
+package org.example.minesweeper;
+
+public class Main {
+}
---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class SampleTest {
+    @Test
+    public void sample_test() {
+        assertTrue(true);
+    }
+}
